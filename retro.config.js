73e88b3b7f9efd47f399ca2e39ec633dec489bf9
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2021-04-03 20:43:17 +0900

    Small tweaks

--- retro.config.js
+++ retro.config.js
@@ -1,22 +1,10 @@
 const sass = {
 	name: "scss",
 	setup(build) {
-		const path = require("path")
 		const sass = require("sass")
 
-		build.onResolve({ filter: /\.scss$/ }, args => ({
-			path: args.path,
-			namespace: "scss-ns",
-		}))
-
-		// const contents = require("child_process")
-		// 	.execSync(`sass ${path.join(process.env.SRC_DIR, args.path)} --color`, { stdio: "inherit" })
-		// 	.toString()
-
-		build.onLoad({ filter: /.*/, namespace: "scss-ns" }, args => {
-			const result = sass.renderSync({
-				file: path.join(process.env.SRC_DIR, args.path),
-			})
+		build.onLoad({ filter: /\.scss$/ }, args => {
+			const result = sass.renderSync({ file: args.path })
 			return {
 				contents: result.css.toString(),
 				loader: "css",
@@ -26,9 +14,5 @@ const sass = {
 }
 
 module.exports = {
-	define: {
-		__DEV__: process.env.ENV === "development",
-		__SSR__: false,
-	},
 	plugins: [sass],
 }
